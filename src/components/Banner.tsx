dedeb373deb13dd9caf4f5d5f103f40889c03cf3
Jonathan Rodríguez <trymilenio@hotmail.com>
2022-03-05 18:46:04 +0000

    Added image and applied filters to the banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react';
+import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
+import banner from '../assets/banner.png';
 
 const Banner: FC = () => {
+  const { t }: { t: Function } = useTranslation('common');
+
   return (
-    <section>
-      Banner
+    <section id="banner" className="h-screen-minus-navbar overflow-hidden" >
+      <div className="w-full h-full filter-custom scale-105">
+        <Image src={banner} alt={t("alt-banner")} layout="fill" />
+      </div>
     </section>
   )
 }
