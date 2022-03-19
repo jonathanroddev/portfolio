9b8a3474ba7a936ecbf3c70d91195a4e80293921
Jonathan Rodríguez <trymilenio@hotmail.com>
2022-03-19 11:43:54 +0000

    Added logos for technologies

--- src/components/DevelopModule.tsx
+++ src/components/DevelopModule.tsx
@@ -10,6 +10,9 @@ const DevelopModule: FC<{ develop: Develop }> = ({ develop }) => {
         <div className="md:w-5/12 w-full md:mb-0 mb-10 relative">
             <div className="flex flex-wrap justify-center content-start absolute w-full h-full overflow-hidden z-10">
                 <h4 className="font-recursive text-4xl text-sky-600 font-normal text-center mb-4 mt-6 bg-slate-200 inline-block h-fit">{t(develop.getTitle)}</h4>
+                <div className="w-full">
+                    <div className="m-auto my-8 rounded-lg border border-black w-1/2 h-64"></div>
+                </div>
             </div>
             <Image src={develop.getBgImage} alt={t(develop.getAltBg)} className="opacity-30" layout="responsive" />
         </div>
