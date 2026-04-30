b92ebe517f29a455e70c75b37c16fdc917f0dc5d
Jonathan Rodríguez <jonathanroddev@gmail.com>
2026-04-30 19:57:18 +0100

    Added Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from "@vercel/analytics/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
 
